e63b1c48cdc6f1aa9695918940764f0dd6a828b4
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-06-21 09:55:27 +0000

    * elementaly MVM support.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/mvm@17503 b2dd03c8-39d4-4d8f-98ff-823fe69b080e
    Signed-off-by: URABE, Shyouhei <shyouhei@ruby-lang.org>

--- include/ruby/vm.h
+++ include/ruby/vm.h
@@ -23,6 +23,22 @@ extern "C" {
 typedef struct rb_vm_struct ruby_vm_t;
 
 /* core API */
+VALUE *ruby_vm_verbose_ptr(ruby_vm_t *);
+VALUE *ruby_vm_debug_ptr(ruby_vm_t *);
+
+VALUE ruby_vm_get_argv(ruby_vm_t*);
+void ruby_vm_set_argv(ruby_vm_t *, int, char **);
+const char *ruby_vm_get_inplace_mode(ruby_vm_t *);
+void ruby_vm_set_inplace_mode(ruby_vm_t *, const char *);
+void ruby_vm_prog_init(ruby_vm_t *);
+VALUE ruby_vm_process_options(ruby_vm_t *, int, char **);
+VALUE ruby_vm_parse_options(ruby_vm_t *, int, char **);
+
+void rb_vm_clear_trace_func(ruby_vm_t *);
+void rb_vm_thread_terminate_all(ruby_vm_t *);
+
+ruby_vm_t *ruby_vm_new(void);
+int ruby_vm_run(ruby_vm_t *, VALUE);
 int ruby_vm_destruct(ruby_vm_t *vm);
 
 #if defined(__cplusplus)
