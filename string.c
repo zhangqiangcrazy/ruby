22f23c312d2fe324068d5a3da223caa70d19b675
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2009-11-19 19:52:15 +0000

    commit miss.
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/mvm@25866 b2dd03c8-39d4-4d8f-98ff-823fe69b080e
    Signed-off-by: URABE, Shyouhei <shyouhei@ruby-lang.org>

--- string.c
+++ string.c
@@ -7617,6 +7617,8 @@ InitVM_String(void)
     rb_define_method(rb_cSymbol, "swapcase", sym_swapcase, 0);
 
     rb_define_method(rb_cSymbol, "encoding", sym_encoding, 0);
+}
+
 
 struct rb_objspace;
 VALUE rb_newobj_from_heap(struct rb_objspace*);
@@ -7635,5 +7637,4 @@ rb_str_copy_to_vm(VALUE str, struct rb_objspace *objspace)
 	ptr[size] = 0;
     }
     return copy;
-}
 }
