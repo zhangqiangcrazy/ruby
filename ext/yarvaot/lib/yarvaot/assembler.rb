6daf81edccb92c2c9b1a1d4da6bcf18d2214b00f
Urabe, Shyuohei <shyouhei@ruby-lang.org>
2010-01-23 23:24:27 +0900

    implement of preprocessor

---
+++ ext/yarvaot/lib/yarvaot/assembler.rb
@@ -0,0 +1,27 @@
+#! /some/path/to/ruby
+# coding=utf-8
+
+# Ruby to C  (and then, to machine executable)  compiler, originally written by
+# Urabe  Shyouhei <shyouhei@ruby-lang.org>  during 2010.   See the  COPYING for
+# legal info.
+
+# This is the assembler, C->object transformation engine.
+class YARVAOT::Assembler < YARVAOT::Subcommand
+
+	# Instantiate.  Does nothing yet.
+	def initialize
+		super
+	end
+end
+
+# 
+# Local Variables:
+# mode: ruby
+# coding: utf-8
+# indent-tabs-mode: t
+# tab-width: 3
+# ruby-indent-level: 3
+# fill-column: 79
+# default-justification: full
+# End:
+# vi: ts=3 sw=3
