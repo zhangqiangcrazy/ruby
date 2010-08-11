f6b85746350509aa33b064ed5e808b28a120c3d0
Shugo Maeda <shugo@ruby-lang.org>
2010-08-11 18:41:54 +0900

    redefine optimized methods for classbox.

--- lib/classbox.rb
+++ lib/classbox.rb
@@ -14,11 +14,24 @@ class Classbox < Module
     @__overlayed_modules__ = {}
   end
 
+  @@__optimized_methods__ = [
+    :+, :-, :*, :/, :%, :==, :===, :<, :<=, :<<, :[], :[]=, :>, :>=, :!, :!=,
+    :length, :size, :succ
+  ]
+
   def refine(klass, &block)
     mod = Module.new
     modules = @__overlayed_modules__[klass] ||= []
     modules.push(mod)
     overlay_module(klass, mod, binding(1))
+    mod.singleton_class.send(:define_method, :method_added) do |mid|
+      if @@__optimized_methods__.include?(mid)
+        klass.send(:alias_method, :__original_optimized_method__, mid)
+        klass.send(:define_method, mid) {}
+        klass.send(:alias_method, mid, :__original_optimized_method__)
+        klass.send(:remove_method, :__original_optimized_method__)
+      end
+    end
     mod.module_eval(&block)
   end
 end
