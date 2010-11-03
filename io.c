tmm1/matzruby

* dir.c (rb_dir_open, rb_dir_fchmod, rb_dir_lchmod, rb_dir_fchown),   (rb_dir_lchown, rb_dir_fstat, rb_dir_lstat, rb_dir_utime),   (rb_dir_mkdir, rb_dir_rmdir, rb_dir_unlink, rb_dir_chdir): added.
* file.c (rb_file_stat_new, rb_stat, ruby_futimesat): made public.

* io.c (rb_openat): added.


git-svn-id: http://svn.ruby-lang.org/repos/ruby/branches/mvm@17920 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

io.c
@@ -3549,6 +3549,67 @@ rb_file_sysopen(const char *fname, int flags, int mode)
     return rb_file_sysopen_internal(io_alloc(rb_cFile), fname, flags, mode);
 }
 
+VALUE
+rb_openat(int argc, VALUE *argv, int base, const char *path)
+{
+    VALUE fname, vmode, perm, newio;
+    volatile VALUE full;
+    int flags = O_RDONLY, fmode = 0666, fd;
+    const char *fullpath;
+#ifdef AT_FDCWD
+    const char *name;
+#endif
+    rb_io_t *fptr;
+
+    rb_secure(2);
+    rb_scan_args(argc, argv, "12", &fname, &vmode, &perm);
+    FilePathValue(fname);
+
+    if (!NIL_P(vmode)) {
+	if (FIXNUM_P(vmode)) {
+	    flags = FIX2INT(vmode);
+	}
+	else {
+	    SafeStringValue(vmode);
+	    flags = rb_io_mode_modenum(RSTRING_PTR(vmode));
+	}
+    }
+    if (!NIL_P(perm)) {
+	fmode = NUM2INT(perm);
+    }
+    newio = io_alloc(rb_cFile);
+    MakeOpenFile(newio, fptr);
+    fptr->mode = rb_io_modenum_flags(flags);
+#ifdef AT_FDCWD
+    name = RSTRING_PTR(fname);
+#define RB_DO_OPENAT() ((base == -1) ?			\
+			open(fullpath, flags, fmode) :	\
+			openat(base, name, flags, fmode))
+#else
+#define RB_DO_OPENAT() open(fullpath, flags, fmode)
+#endif
+    full = fname = rb_file_expand_path(fname, rb_str_new2(path));
+    RBASIC(fname)->klass = 0;
+    OBJ_FREEZE(fname);
+    fullpath = RSTRING_PTR(fname);
+    if ((fd = RB_DO_OPENAT()) < 0) {
+	if (errno == EMFILE || errno == ENFILE) {
+	    rb_gc();
+	    fd = RB_DO_OPENAT();
+	}
+	if (fd < 0) {
+	    rb_sys_fail(fullpath);
+	}
+    }
+#undef RB_DO_OPENAT
+    fptr->fd = fd;
+    fptr->path = strdup(fullpath);
+    if (rb_block_given_p()) {
+	return rb_ensure(rb_yield, newio, io_close, newio);
+    }
+    return newio;
+}
+
 #if defined(__CYGWIN__) || !defined(HAVE_FORK)
 static struct pipe_list {
     rb_io_t *fptr;

zhangqiangcrazy/ruby

git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/mvm@17920 b2dd03c8-39d4-4d8f-98ff-823fe69b080e
Signed-off-by: URABE, Shyouhei <shyouhei@ruby-lang.org>

io.c
@@ -4728,6 +4728,67 @@ rb_file_open(const char *fname, const char *modestr)
     return rb_file_open_internal(io_alloc(rb_cFile), rb_str_new_cstr(fname), modestr);
 }
 
+VALUE
+rb_openat(int argc, VALUE *argv, int base, const char *path)
+{
+    VALUE fname, vmode, perm, newio;
+    volatile VALUE full;
+    int flags = O_RDONLY, fmode = 0666, fd;
+    const char *fullpath;
+#ifdef AT_FDCWD
+    const char *name;
+#endif
+    rb_io_t *fptr;
+
+    rb_secure(2);
+    rb_scan_args(argc, argv, "12", &fname, &vmode, &perm);
+    FilePathValue(fname);
+
+    if (!NIL_P(vmode)) {
+	if (FIXNUM_P(vmode)) {
+	    flags = FIX2INT(vmode);
+	}
+	else {
+	    SafeStringValue(vmode);
+	    flags = rb_io_modestr_oflags(RSTRING_PTR(vmode));
+	}
+    }
+    if (!NIL_P(perm)) {
+	fmode = NUM2INT(perm);
+    }
+    newio = io_alloc(rb_cFile);
+    MakeOpenFile(newio, fptr);
+    fptr->mode = rb_io_oflags_fmode(flags);
+#ifdef AT_FDCWD
+    name = RSTRING_PTR(fname);
+#define RB_DO_OPENAT() ((base == -1) ?			\
+			open(fullpath, flags, fmode) :	\
+			openat(base, name, flags, fmode))
+#else
+#define RB_DO_OPENAT() open(fullpath, flags, fmode)
+#endif
+    full = fname = rb_file_expand_path(fname, rb_str_new2(path));
+    RBASIC(fname)->klass = 0;
+    OBJ_FREEZE(fname);
+    fullpath = RSTRING_PTR(fname);
+    if ((fd = RB_DO_OPENAT()) < 0) {
+	if (errno == EMFILE || errno == ENFILE) {
+	    rb_gc();
+	    fd = RB_DO_OPENAT();
+	}
+	if (fd < 0) {
+	    rb_sys_fail_path(fname);
+	}
+    }
+#undef RB_DO_OPENAT
+    fptr->fd = fd;
+    fptr->pathv = fname;
+    if (rb_block_given_p()) {
+	return rb_ensure(rb_yield, newio, io_close, newio);
+    }
+    return newio;
+}
+
 #if defined(__CYGWIN__) || !defined(HAVE_FORK)
 static struct pipe_list {
     rb_io_t *fptr;