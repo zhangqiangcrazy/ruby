tmm1/matzruby

* vm_core.h (rb_thread_t): per-thread cwd support.  [EXPERIMENTAL]
* io.c (rb_sysopen), dir.c (dir_s_chdir),
  file.c (ruby_readlink, ruby_absolute_path_p),
  thread.c (thread_create_core, ruby_thread_getcwd, ruby_getcwd),
  vm.c (Init_VM): ditto.

* string.c (rb_str_wrap): wrap a heap pointer with string.

* thread.c (ruby_system_alone): returns if only current VM is running.


git-svn-id: http://svn.ruby-lang.org/repos/ruby/branches/mvm@18021 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

file.c
@@ -2256,6 +2256,41 @@ rb_file_s_symlink(VALUE klass, VALUE from, VALUE to)
 #endif
 }
 
+#ifdef HAVE_READLINK
+char *
+ruby_readlink(const char *path, long *len)
+{
+    char *buf, *tmp;
+    int size = 100;
+    int rv;
+ 
+    buf = malloc(size);
+    if (!buf) return 0;
+    while ((rv = readlink(path, buf, size)) == size
+#ifdef _AIX
+	    || (rv < 0 && errno == ERANGE) /* quirky behavior of GPFS */
+#endif
+	) {
+	size *= 2;
+	tmp = realloc(buf, size);
+	if (!tmp) {
+	    free(buf);
+	    return 0;
+	}
+    }
+    if (rv < 0) {
+	free(buf);
+	return 0;
+    }
+    buf[rv] = '\0';
+    if (size > rv + 1) {
+	buf = realloc(buf, rv + 1);
+    }
+    *len = rv;
+    return buf;
+}
+#endif
+
 /*
  *  call-seq:
  *     File.readlink(link_name) -> file_name
@@ -2272,29 +2307,15 @@ rb_file_s_readlink(VALUE klass, VALUE path)
 {
 #ifdef HAVE_READLINK
     char *buf;
-    int size = 100;
-    int rv;
-    VALUE v;
+    long rv;
 
     rb_secure(2);
     FilePathValue(path);
-    buf = xmalloc(size);
-    while ((rv = readlink(RSTRING_PTR(path), buf, size)) == size
-#ifdef _AIX
-	    || (rv < 0 && errno == ERANGE) /* quirky behavior of GPFS */
-#endif
-	) {
-	size *= 2;
-	buf = xrealloc(buf, size);
-    }
-    if (rv < 0) {
-	xfree(buf);
+    buf = ruby_readlink(RSTRING_PTR(path), &rv);
+    if (!buf) {
 	rb_sys_fail(RSTRING_PTR(path));
     }
-    v = rb_tainted_str_new(buf, rv);
-    xfree(buf);
-
-    return v;
+    return rb_str_wrap(buf, rv);
 #else
     rb_notimplement();
     return Qnil;		/* not reached */
@@ -2608,7 +2629,8 @@ ntfs_tail(const char *path)
     (void)(extenc || (extenc = rb_default_external_encoding())),\
     rb_enc_associate(result, extenc))
 
-static int is_absolute_path(const char*);
+#define is_absolute_path(path) ruby_absolute_path_p(path)
+int ruby_absolute_path_p(const char*);
 
 static VALUE
 file_expand_path(VALUE fname, VALUE dname, VALUE result)
@@ -4347,8 +4369,8 @@ rb_file_const(const char *name, VALUE value)
     rb_define_const(rb_mFConst, name, value);
 }
 
-static int
-is_absolute_path(const char *path)
+int
+ruby_absolute_path_p(const char *path)
 {
 #ifdef DOSISH_DRIVE_LETTER
     if (has_drive_letter(path) && isdirsep(path[2])) return 1;

io.c
@@ -3395,7 +3395,12 @@ rb_io_mode_enc(rb_io_t *fptr, const char *mode)
 }
 
 struct sysopen_struct {
-    char *fname;
+#if USE_OPENAT
+    int base;
+#else
+    VALUE fullpath;
+#endif
+    const char *fname;
     int flag;
     unsigned int mode;
 };
@@ -3404,29 +3409,50 @@ static VALUE
 sysopen_func(void *ptr)
 {
     struct sysopen_struct *data = ptr;
+#if USE_OPENAT
+    return (VALUE)openat(data->base, data->fname, data->flag, data->mode);
+#else
     return (VALUE)open(data->fname, data->flag, data->mode);
+#endif
+}
+
+static void
+rb_sysopen_prepare(struct sysopen_struct *data, const char *fname, int flags, unsigned int mode)
+{
+#if USE_OPENAT
+    data->base = GET_THREAD()->cwd.fd;
+#else
+    if (ruby_absolute_path_p(fname)) {
+	data->base = Qnil;
+    }
+    else {
+	data->base = rb_file_expand_path(rb_str_new2(fname), Qnil);
+	fname = RSTRING_PTR(data->base);
+    }
+#endif
+    data->fname = fname;
+    data->flag = flags;
+    data->mode = mode;
 }
 
 static int
-rb_sysopen_internal(char *fname, int flags, unsigned int mode)
+rb_sysopen_internal(struct sysopen_struct *data)
 {
-    struct sysopen_struct data;
-    data.fname = fname;
-    data.flag = flags;
-    data.mode = mode;
-    return (int)rb_thread_blocking_region(sysopen_func, &data, RB_UBF_DFL, 0);
+    return (int)rb_thread_blocking_region(sysopen_func, data, RB_UBF_DFL, 0);
 }
 
 static int
 rb_sysopen(char *fname, int flags, unsigned int mode)
 {
     int fd;
+    struct sysopen_struct data;
 
-    fd = rb_sysopen_internal(fname, flags, mode);
+    rb_sysopen_prepare(&data, fname, flags, mode); 
+    fd = rb_sysopen_internal(&data);
     if (fd < 0) {
 	if (errno == EMFILE || errno == ENFILE) {
 	    rb_gc();
-	    fd = rb_sysopen_internal(fname, flags, mode);
+	    fd = rb_sysopen_internal(&data);
 	}
 	if (fd < 0) {
 	    rb_sys_fail(fname);

zhangqiangcrazy/ruby

git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/mvm@18021 b2dd03c8-39d4-4d8f-98ff-823fe69b080e
Signed-off-by: URABE, Shyouhei <shyouhei@ruby-lang.org>

file.c
@@ -2424,6 +2424,35 @@ rb_file_s_symlink(VALUE klass, VALUE from, VALUE to)
 #endif
 
 #ifdef HAVE_READLINK
+char *
+ruby_readlink(const char *path, long *len)
+{
+    char *buf;
+    int size = 100;
+    int rv;
+ 
+    buf = xmalloc(size);
+    if (!buf) return 0;
+    while ((rv = readlink(path, buf, size)) == size
+#ifdef _AIX
+	    || (rv < 0 && errno == ERANGE) /* quirky behavior of GPFS */
+#endif
+	) {
+	size *= 2;
+	buf = xrealloc(buf, size);
+    }
+    if (rv < 0) {
+	xfree(buf);
+	return 0;
+    }
+    buf[rv] = '\0';
+    if (size > rv + 1) {
+	buf = xrealloc(buf, rv + 1);
+    }
+    *len = rv;
+    return buf;
+}
+
 /*
  *  call-seq:
  *     File.readlink(link_name)  ->  file_name
@@ -2439,30 +2468,16 @@ static VALUE
 rb_file_s_readlink(VALUE klass, VALUE path)
 {
     char *buf;
-    int size = 100;
-    ssize_t rv;
-    VALUE v;
+    long rv;
 
     rb_secure(2);
     FilePathValue(path);
     path = rb_str_encode_ospath(path);
-    buf = xmalloc(size);
-    while ((rv = readlink(RSTRING_PTR(path), buf, size)) == size
-#ifdef _AIX
-	    || (rv < 0 && errno == ERANGE) /* quirky behavior of GPFS */
-#endif
-	) {
-	size *= 2;
-	buf = xrealloc(buf, size);
+    buf = ruby_readlink(RSTRING_PTR(path), &rv);
+    if (!buf) {
+	rb_sys_fail(RSTRING_PTR(path));
     }
-    if (rv < 0) {
-	xfree(buf);
-	rb_sys_fail_path(path);
-    }
-    v = rb_tainted_str_new(buf, rv);
-    xfree(buf);
-
-    return v;
+    return rb_str_wrap(buf, rv);
 }
 #else
 #define rb_file_s_readlink rb_f_notimplement
@@ -2850,6 +2865,9 @@ rb_home_dir(const char *user, VALUE result)
     rb_enc_associate_index(result, rb_filesystem_encindex());
     return result;
 }
+
+#define is_absolute_path(path) ruby_absolute_path_p(path)
+int ruby_absolute_path_p(const char*);
 
 static VALUE
 file_expand_path(VALUE fname, VALUE dname, int abs_mode, VALUE result)
@@ -4911,7 +4929,7 @@ rb_file_const(const char *name, VALUE value)
 }
 
 int
-rb_is_absolute_path(const char *path)
+ruby_absolute_path_p(const char *path)
 {
 #ifdef DOSISH_DRIVE_LETTER
     if (has_drive_letter(path) && isdirsep(path[2])) return 1;

io.c
@@ -4490,6 +4490,11 @@ rb_io_extract_modeenc(VALUE *vmode_p, VALUE *vperm_p, VALUE opthash,
 }
 
 struct sysopen_struct {
+#if USE_OPENAT
+    int base;
+#else
+    VALUE fullpath;
+#endif
     VALUE fname;
     int oflags;
     mode_t perm;
@@ -4500,7 +4505,33 @@ sysopen_func(void *ptr)
 {
     const struct sysopen_struct *data = ptr;
     const char *fname = RSTRING_PTR(data->fname);
+#if USE_OPENAT
+    return (VALUE)openat(data->base, fname, data->oflags, data->perm);
+#else
     return (VALUE)open(fname, data->oflags, data->perm);
+#endif
+}
+
+static void
+rb_sysopen_prepare(struct sysopen_struct *data, VALUE fname, int oflags, mode_t perm)
+{
+#ifdef O_BINARY
+    oflags |= O_BINARY;
+#endif
+
+#if USE_OPENAT
+    data->base = GET_THREAD()->cwd.fd;
+#else
+    if (ruby_absolute_path_p(RSTRING_PTR(fname))) {
+	data->base = Qnil;
+    }
+    else {
+	data->base = fname = rb_file_expand_path(fname, Qnil);
+    }
+#endif
+    data->fname = rb_str_encode_ospath(fname);
+    data->oflags = oflags;
+    data->perm = perm;
 }
 
 static inline int
@@ -4515,13 +4546,7 @@ rb_sysopen(VALUE fname, int oflags, mode_t perm)
     int fd;
     struct sysopen_struct data;
 
-#ifdef O_BINARY
-    oflags |= O_BINARY;
-#endif
-    data.fname = rb_str_encode_ospath(fname);
-    data.oflags = oflags;
-    data.perm = perm;
-
+    rb_sysopen_prepare(&data, fname, oflags, perm); 
     fd = rb_sysopen_internal(&data);
     if (fd < 0) {
 	if (errno == EMFILE || errno == ENFILE) {