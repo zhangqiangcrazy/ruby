tmm1/matzruby

* ruby.c (process_options): set script_name directly and translate   before frozen.
* ruby.c (set_arg0): now setter of VM specific variable is called for
  the pointer instead of the key.


git-svn-id: http://svn.ruby-lang.org/repos/ruby/branches/mvm@18103 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

ruby.c
@@ -1062,11 +1062,12 @@ process_options(VALUE arg)
 	}
     }
 
-    ruby_script(opt->script);
+    opt->script_name = rb_tainted_str_new2(opt->script);
+    *rb_vm_specific_ptr(rb_vmkey_progname) = opt->script_name;
 #if defined DOSISH || defined __CYGWIN__
-    translate_char(RSTRING_PTR(*ruby_vm_specific_ptr(vm, rb_vmkey_progname)), '\\', '/');
+    translate_char(RSTRING_PTR(opt->script_name), '\\', '/');
 #endif
-    opt->script_name = rb_str_new4(*ruby_vm_specific_ptr(vm, rb_vmkey_progname));
+    OBJ_FREEZE(opt->script_name);
     opt->script = RSTRING_PTR(opt->script_name);
     ruby_vm_set_argv(vm, argc, argv);
     process_sflag(vm, opt);
@@ -1395,10 +1396,9 @@ set_arg0(VALUE val, ID id, VALUE *var)
 	}
     }
 #endif
-    *rb_vm_specific_ptr((int)var) = rb_obj_freeze(rb_tainted_str_new(s, i));
+    *var = rb_obj_freeze(rb_tainted_str_new(s, i));
 }
 
-DEPRECATED(void ruby_script(const char *name));
 void
 ruby_script(const char *name)
 {

zhangqiangcrazy/ruby

git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/branches/mvm@18103 b2dd03c8-39d4-4d8f-98ff-823fe69b080e
Signed-off-by: URABE, Shyouhei <shyouhei@ruby-lang.org>

ruby.c
@@ -1307,7 +1307,7 @@ process_options(rb_vm_t *vm, long argc, char **argv, struct cmdline_options *opt
     }
 
     opt->script_name = rb_str_new_cstr(opt->script);
-    opt->script = RSTRING_PTR(opt->script_name);
+    *rb_vm_specific_ptr(rb_vmkey_progname) = opt->script_name;
 #if defined DOSISH || defined __CYGWIN__
     translit_char(RSTRING_PTR(opt->script_name), '\\', '/');
 #endif
@@ -1755,11 +1755,9 @@ set_arg0(VALUE val, ID id, VALUE *var)
 	}
     }
 #endif
-
-    *rb_vm_specific_ptr((int)var) = rb_obj_freeze(rb_external_str_new(s, i));
+    *var = rb_obj_freeze(rb_external_str_new(s, i));
 }
 
-DEPRECATED(void ruby_script(const char *name));
 void
 ruby_script(const char *name)
 {