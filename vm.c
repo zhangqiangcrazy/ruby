cc9b3224abfd0de88ebe37723872663f9fe6619a
URABE, Shyouhei <shyouhei@ruby-lang.org>
2010-12-08 21:01:53 +0900

    * vm.c (ruby_vmptr_destruct): vm->mark_object_ary shall be flushed   prior to rb_objspace_free, otherwise objects might remain alive.

--- vm.c
+++ vm.c
@@ -1572,6 +1572,7 @@ ruby_vmptr_destruct(rb_vm_t *vm)
 	rb_queue_destroy(&vm->queue.signal);
         rb_objspace_xfree(vm->objspace, vm->cache);
         rb_objspace_xfree(vm->objspace, vm->specific_storage.ptr);
+        rb_ary_clear(vm->mark_object_ary);
         rb_objspace_free(vm->objspace);
         free(vm->init_options);
 	return TRUE;
